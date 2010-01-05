d3964138d388a5a83206749b964b49a33e23b045
ikarpov <ikarpov@gmail.com>
2010-01-05 02:48:04 +0000

    merge branches/open-windows - add help buttons and F1 buttons in mods

--- mods/hub/client.py
+++ mods/hub/client.py
@@ -1,8 +1,8 @@
 from OpenNero import *
 from nero_mod import list_mods
+from inputConfig import *
 
-from common import gui
-from common import getGuiManager
+from common import gui, getGuiManager, openWiki
 
 class SwitchMod:
     def __init__(self, mod, path):
@@ -12,17 +12,13 @@ class SwitchMod:
         switchMod(self.mod, self.path)
 
 def ClientMain():
-    
-    # don't show physics
-    # disable_physics()
-
     # set up the gui
     guiMan = getGuiManager()
     guiMan.setTransparency(1.0)
     guiMan.setFont("data/gui/fonthaettenschweiler.bmp")
 
     neroImg = gui.create_image( guiMan, 'nero_background', Pos2i(0,0), Pos2i(0,0), 'data/gui/nero_background.jpg' )
-    
+
     mods = list_mods()
     for i, modname in enumerate(mods):
         button = gui.create_button(guiMan, modname, Pos2i(325, 100 + i * 100), Pos2i(100, 50), '')
@@ -33,3 +29,7 @@ def ClientMain():
     killButton = gui.create_button( guiMan, 'kill', Pos2i(125,100), Pos2i(100,50), '' )
     killButton.text = 'Exit'
     killButton.OnMouseLeftClick = lambda: getSimContext().killGame()
+
+    helpButton = gui.create_button( guiMan, 'help', Pos2i(125,175), Pos2i(100,50), '' )
+    helpButton.text = 'Help'
+    helpButton.OnMouseLeftClick = openWiki('RunningOpenNero')
