aed5ccb66f3b001f66869e2404852e2e6708369e
ikarpov <ikarpov@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2010-01-19 06:59:07 +0000

    client/server working

---
+++ trunk/mods/plot_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""Process and display log files from OpenNERO.
+
+plot_client reads a file and sends it over the network for plot_server to process.
+"""
+
+import sys
+import socket
+
+HOST, PORT = "localhost", 9999
+ADDR = (HOST, PORT)
+BUFSIZE = 4096
+
+def main():
+    # Create socket
+    UDPSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    f = sys.stdin
+    if len(sys.argv) > 1:
+        f = open(sys.argv[1])
+    # Send messages
+    for line in f.xreadlines():
+        line = line.strip()
+        UDPSock.sendto(line,ADDR)
+    f.close()
+    UDPSock.close()
+
+if __name__ == "__main__":
+    main()
