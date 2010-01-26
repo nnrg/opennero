8e3e028e6d9dedc758e611a2b761427e28121ccc
ikarpov <ikarpov@gmail.com>
2010-01-26 16:04:57 +0000

    allow use of start-command and remove C++-side command line argument parsing

--- source/app.cpp
+++ source/app.cpp
@@ -8,12 +8,11 @@
 #include "core/ONTime.h"
 #include "game/SimContext.h"
 #include "game/Kernel.h"
+#include "scripting/scripting.h"
 
 #include "audio/AudioManager.h"
 #include "utils/Performance.h"
 #include "utils/Config.h"
-
-#include "tclap/CmdLine.h"
 
 namespace OpenNero
 {
@@ -31,9 +30,6 @@ namespace OpenNero
 
     /// value returned on error
     static const int32_t     kErrorReturn           = -1;
-
-    /// Parse a command line into key/value map
-    CommandLineParams parse_command_line(int argc, char* argv[]);
 
     /// entrance into the OpenNero application
     /// @param argc number of arguments passed in
@@ -84,6 +80,10 @@ namespace OpenNero
         OpenNero::Kernel&		kern = OpenNero::Kernel::instance();
         kern.Initialize(irrDevice, appConfig, argc, argv);
         kern.switchMod( irrDevice, appConfig.start_mod_name, appConfig.start_mod_dir );
+        if (!appConfig.start_command.empty())
+        {
+        	ScriptingEngine::instance().Exec(appConfig.start_command);
+        }
 
         // run the loop until the device is killed
         while(irrDevice->run())
@@ -112,43 +112,6 @@ namespace OpenNero
         OpenNero::Log::LogSystemShutdown();
 
         return 0;
-    }
-
-    /**
-     * Parse command line parameters
-     * @param argc number of command line parameters
-     * @param argv array of command line parameters including command name (argv[0])
-     * @return map of parameter names and values
-     */
-    CommandLineParams parse_command_line(int argc, char* argv[])
-    {
-        CommandLineParams results;
-
-        TCLAP::CmdLine cmd("OpenNERO (c) 2008 The University of Texas at Austin", ' ', "0.1");
-
-        TCLAP::ValueArg<std::string> argSeeds("s","seeds","Random map seeds", false, "12345", "string");
-        cmd.add(argSeeds);
-        TCLAP::ValueArg<std::string> argConfig("c","config","Configuration file", false, "appConfig.py", "string");
-        cmd.add(argConfig);
-
-        // Wrap everything in a try block.  Do this every time,
-        // because exceptions will be thrown for problems.
-        try {
-#if !NERO_PLATFORM_MAC
-            // Parse the argv array, unless we are on a mac, in which case we are probably running from within a bunlde
-            cmd.parse( argc, argv );
-#endif
-        }
-        catch (TCLAP::ArgException &e)
-        {
-            LOG_F_ERROR("core", "Error: " << e.error() << " for arg " << e.argId());
-        }
-
-        // Get the value parsed by each arg.
-        results["seeds"] = argSeeds.getValue();
-        results["config"] = argConfig.getValue();
-
-        return results;
     }
 
 } // end OpenNero
