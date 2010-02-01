220ec8878a8b099aa9d578d0b9169e0be180194e
ikarpov <ikarpov@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2010-02-01 19:58:53 +0000

    refresh branch

--- branches/rtneat/mods/NERO/main.py
+++
@@ -1,9 +0,0 @@
-#import all client and server scripts
-from NERO.module import *
-from NERO.module import getMod
-from NERO.client import *
-
-def ModMain():
-    print "In Mod Main"
-    ServerMain()
-    ClientMain()  
--- branches/rtneat/mods/Roomba/main.py
+++
@@ -1,7 +0,0 @@
-#import all client and server scripts
-from Roomba.module import *
-from Roomba.client import *
-
-def ModMain():
-    ServerMain()
-    ClientMain()  
