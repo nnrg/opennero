9d128ce5b37550a17235b500b8aaffed094f28a9
ikarpov <ikarpov@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2010-02-09 14:54:09 +0000

    warnings

--- branches/nero/source/ai/rtneat/rtNEAT.h
+++ branches/nero/source/ai/rtneat/rtNEAT.h
@@ -35,7 +35,7 @@ namespace OpenNero
         /// @param filename name of the file with the initial population genomes
         /// @param param_file file with RTNEAT parameters to load
         /// @param population_size size of the population to construct
-        RTNEAT(const std::string& filename, const std::string& param_file, S32 population_size);
+        RTNEAT(const std::string& filename, const std::string& param_file, size_t population_size);
 
         /// Constructor
         /// @param param_file RTNEAT parameter file
@@ -43,7 +43,7 @@ namespace OpenNero
         /// @param outputs number of outputs
         /// @param population_size size of the population to construct
         /// @param noise variance of the Gaussian used to assign initial weights
-        RTNEAT(const std::string& param_file, S32 inputs, S32 outputs, S32 population_size, F32 noise);
+        RTNEAT(const std::string& param_file, size_t inputs, size_t outputs, size_t population_size, F32 noise);
 
         /// Destructor
         ~RTNEAT();
