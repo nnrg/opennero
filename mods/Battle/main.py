53089b293368ef5bbe1507da7d99d9b7f706cf2e
ikarpov <ikarpov@gmail.com>
2010-09-09 22:07:59 +0000

    merge external menu, NERO mod improvements, and Battle mod from branches/nero

---
+++ mods/Battle/main.py
@@ -0,0 +1,10 @@
+#Note this mod is intended for use in competetive open nero tornaments, and therefore lacks many of the key features of the NERO mod
+
+#import all client and server scripts
+from Battle.module import *
+from Battle.module import getMod
+from Battle.client import *
+
+def ModMain():
+    ServerMain()
+    ClientMain()  
