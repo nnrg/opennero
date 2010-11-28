3c9d3a56eca555b5f7b0ba5b98f826bee43868e6
ikarpov <ikarpov@gmail.com>
2010-11-28 05:39:46 +0000

    fix windows build of testOpenNERO target

--- test/ai/sensors/TestSensors.cpp
+++ test/ai/sensors/TestSensors.cpp
@@ -6,8 +6,10 @@
 #include <boost/archive/xml_iarchive.hpp>
 #include <boost/serialization/vector.hpp>
 #include <boost/serialization/shared_ptr.hpp>
-#include <boost/test/unit_test.hpp>
 #include <sstream>
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE( test_opennero )
 
--- test/scripting/TestExports.cpp
+++ test/scripting/TestExports.cpp
@@ -1,5 +1,5 @@
 #include "scripting/exports.h"
-
+#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE( test_opennero )
