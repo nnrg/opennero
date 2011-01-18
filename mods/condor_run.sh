15616d9b24a0676b6ff7c26049f41ccd5caf0c26
AdamDziuk <adamdziuk@users.noreply.github.com>
2011-01-18 08:19:11 +0000

    Further adjustments

--- mods/condor_run.sh
+++ mods/condor_run.sh
@@ -7,13 +7,7 @@ RUN=$1
 cd $SCRIPT_DIR
 
 # start child process
-./OpenNERO --log opennero_condor_${RUN}.log --mod Maze --modpath Maze:common --headless --command "StartMe()" > /dev/null &
+export RUN=${RUN}
+./OpenNERO --log opennero_condor_${RUN}.log --mod Blocksworld --modpath Blocksworld:common --headless > /dev/null &
 OPENNERO_PID=$!
 echo STARTED OpenNERO with PID $OPENNERO_PID
-
-# sleep for some time
-sleep 30s
-
-# kill child process
-kill -HUP $OPENNERO_PID
-echo KILLED OpenNERO PID $OPENNERO_PID
