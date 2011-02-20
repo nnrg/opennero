1f95899fc6ff0c94b1d9a9bedf7ca8a34e16c1cf
ikarpov <ikarpov@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2011-02-20 07:57:05 +0000

    wrong name

--- factory2/mods/experiment.py
+++
@@ -1,7 +0,0 @@
-# Automated experiment script
-# 1) Change this file with initial commands to be run within OpenNERO
-# 2) Start OpenNERO in headless mode like this (Linux):
-#    ./OpenNERO --no-window --mod-path=Maze:common --mod=Maze --command="import experiment"
-# Example:
-# from Maze.module import getMod
-# getMod().start_sarsa()
