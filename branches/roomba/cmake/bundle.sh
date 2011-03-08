e5662950a3fa5519afaefc1f8e7d98d49e7322ff
ikarpov <ikarpov@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2011-03-08 16:51:34 +0000

    merged into trunk

--- branches/roomba/cmake/bundle.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp -r $1/build $2/Contents/Resources && (cd $2/Contents/Resources; find . | grep .svn$ | xargs rm -r )
-cp -r $1/external/lib/macOS $2/Contents/Frameworks && (cd $2/Contents/Frameworks ; find . | grep .svn$ | xargs rm -r )
-true
