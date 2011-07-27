a782a00cb765cb285c3c42c87bb4c87796ec3d41
ikarpov <ikarpov@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2011-07-27 16:39:28 +0000

    changes merged into trunk

--- branches/animation_bug/mods/NERO_Battle/constants.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-from NERO.constants import *
-
-# number of steps per lifetime
-# in battle, the agents can only die by being shot and so their lifetime is
-# maxint
-lifetime = sys.maxint
