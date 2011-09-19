3a395adbefa684076efaea958807327420ed4857
ikarpov <ikarpov@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2011-09-19 18:17:25 +0000

    branch for rtneat patches

---
+++ branches/rtneat/test/scripting/TestExports.cpp
@@ -0,0 +1,11 @@
+#include "scripting/exports.h"
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE( test_opennero )
+
+BOOST_AUTO_TEST_CASE( test_exports )
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
