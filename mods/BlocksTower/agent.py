1cf539218d15c8ce119f54e002a203b4c6564e43
ikarpov <ikarpov@gmail.com>
2011-11-03 22:43:51 +0000

    merge branches/towers3

--- mods/BlocksTower/agent.py
+++ mods/BlocksTower/agent.py
@@ -4,22 +4,7 @@ import BlocksTower
 import random
 from BlocksTower.environment import TowerEnvironment
 from BlocksTower.constants import *
-
-def get_action_index(move):
-    if move in TowerEnvironment.MOVES:
-        action = TowerEnvironment.MOVES.index(move)
-        print 'Picking action', action, 'for move', move
-        return action
-    else:
-        return None
-
-class Cell:
-    def __init__(self, h, r, c):
-        self.h = h
-        self.r = r
-        self.c = c
-    def __cmp__(self, other):
-        return cmp(self.h, other.h)
+from copy import copy
 
 ###
 #
@@ -33,14 +18,32 @@ class Cell:
 #
 ###
 
-
 class TowerAgent(AgentBrain):
     """
     An agent designed to solve Towers of Hanoi
     """
     def __init__(self):
         AgentBrain.__init__(self) # have to make this call
-        self.queue_init()
+        
+    def move(self, frm, to):
+        if frm == 'a' and to == 'b': return self.atob
+        if frm == 'a' and to == 'c': return self.atoc
+        if frm == 'b' and to == 'a': return self.btoa
+        if frm == 'b' and to == 'c': return self.btoc
+        if frm == 'c' and to == 'a': return self.ctoa
+        if frm == 'c' and to == 'b': return self.ctob
+
+    def dohanoi(self, n, to, frm, using):
+        if n == 0: return
+        prefix = '\t'.join(['' for i in range(self.num_towers - n)])
+        strn = "Moving depth {n} from {frm} to {to} using {using}".format(n=n, frm=frm, to=to, using=using)
+        for a in self.dohanoi(n-1, using, frm, to):
+            yield a
+        #self.state.label = strn
+        for a in self.move(frm, to):
+            yield a
+        for a in self.dohanoi(n-1, to, using, frm):
+            yield a
 
     def queue_init(self):
         self.init_queue = [1,5]
@@ -53,135 +56,245 @@ class TowerAgent(AgentBrain):
         self.end_queue = [0,0,0,5,5,1]
 
         from module import getMod
-        num_towers = getMod().num_towers
+        self.num_towers = getMod().num_towers
 
-        if num_towers == 3:
-            self.action_queue = self.init_queue
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.atob)
-            self.action_queue += (self.ctob)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.btoa)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.end_queue)
-
-        if num_towers == 4:
-            self.action_queue = self.init_queue
-            self.action_queue += (self.atob)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.atob)
-            self.action_queue += (self.ctoa)
-            self.action_queue += (self.ctob)
-            self.action_queue += (self.atob)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.btoa)
-            self.action_queue += (self.ctoa)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.atob)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.end_queue)
-
-    	if num_towers == 5:
-            self.action_queue = self.init_queue
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.atob)
-            self.action_queue += (self.ctob)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.btoa)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.atob)
-            self.action_queue += (self.ctob)
-            self.action_queue += (self.ctoa)
-            self.action_queue += (self.btoa)
-            self.action_queue += (self.ctob)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.atob)
-            self.action_queue += (self.ctob)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.btoa)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.btoa)
-            self.action_queue += (self.ctob)
-            self.action_queue += (self.ctoa)
-            self.action_queue += (self.btoa)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.atob)
-            self.action_queue += (self.ctob)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.btoa)
-            self.action_queue += (self.btoc)
-            self.action_queue += (self.atoc)
-            self.action_queue += (self.end_queue)
+        #self.state.label = 'Starting to Solve!'
+        for a in self.init_queue:
+            yield a
+        for a in self.dohanoi(self.num_towers, 'b', 'a', 'c'):
+            yield a
+        #self.state.label = 'Problem Solved!'
+        for a in self.end_queue:
+            yield a
 
     def initialize(self,init_info):
         # Create new Agent
         self.action_info = init_info.actions
         return True
+        
+    def display_planner(self):
+        """ show planner internals by running it externally """
+        import subprocess
+        # solve for show (user can click through)
+        subproc = subprocess.Popen(['python', 'BlocksTower/recursive_solver.py'], stdout=subprocess.PIPE)
+        plan = ''
+        while True:
+            try:
+                out = subproc.stdout.read(1)
+            except:
+                break
+            if out == '':
+                break
+            else:
+                plan += out
+        print plan        
 
     def start(self, time, sensors):
         """
         return first action given the first observations
         """
-        return self.action_queue.pop(0)
+        self.display_planner()
+        self.action_queue = self.queue_init()
+        return self.action_queue.next()
 
     def reset(self):
-        self.queue_init()
-
-    def face_at(self,x,y,z,nx,ny):
-        if(x - nx) == -1:
-            return self.face(z,270)
-        if(x - nx) == 1:
-            return self.face(z,90)
-        if(y - ny) == 1:
-            return self.face(z,0)
-        if(y - ny) == -1:
-            return self.face(z,180)
-
-    def face(self,z,nz):
-        if ((z - nz) % 360) == 180:
-            self.action_queue += [4,4]
-        if ((z - nz) % 360) == 90:
-            self.action_queue += [5]
-        if ((z - nz) % 360) == 270:
-            self.action_queue += [4]
-        return nz
-
-    def goto_1(self,x,y,z,nx,ny):
-        if (x - nx) * (x - nx) + (y - ny) * (y - ny) != 1: return (x,y,z)
-        nz = self.face_at(x,y,z,nx,ny)
-        self.action_queue += [1]
-        return (nx,ny,nz)
-
-    def pickup(self):
-        self.action_queue += [3]
-
-    def setdown(self):
-        self.action_queue += [2]
+        self.display_planner()
+        self.action_queue = self.queue_init()
+        return True
 
     def act(self, time, sensors, reward):
         """
         return an action given the reward for the previous action and the new observations
         """
-
-        # Make sure that there's always something on the queue to be popped
-        if len(self.action_queue) == 0:
-            self.action_queue.append(1)
-
-        # Pop the last action and continue
-        return self.action_queue.pop(0)
+        try:
+            return self.action_queue.next()
+        except:
+            return 1
 
     def end(self, time, reward):
         """
         receive the reward for the last observation
         """
         print  "Final reward: %f, cumulative: %f" % (reward[0], self.fitness[0])
+        return True
+
+    def destroy(self):
+        """
+        called when the agent is done
+        """
+        return True
+
+class TowerAgent2(AgentBrain):
+    """
+    An agent that uses a STRIPS-like planner to solve the Towers of Hanoi problem
+    """
+    def __init__(self):
+        AgentBrain.__init__(self) # have to make this call
+        self.action_queue = [5] # rotate left to reset state first
+
+    def initialize(self,init_info):
+        """
+        Create the agent.
+        init_info -- AgentInitInfo that describes the observation space (.sensors),
+                     the action space (.actions) and the reward space (.rewards)
+        """
+        self.action_info = init_info.actions
+        return True
+
+    def queue_init(self):
+        import strips2
+        import towers3 as towers
+        import subprocess
+        import strips2_show
+
+        # solve for show (user can click through)
+        subproc = subprocess.Popen(['python', 'BlocksTower/strips2.py'], stdout=subprocess.PIPE)
+        plan = ''
+        while True:
+            try:
+                out = subproc.stdout.read(1)
+            except:
+                break
+            if out == '':
+                break
+            else:
+                plan += out
+        # actually solve to get the plan of actions
+        plan = strips2.solve(towers.INIT, towers.GOAL, towers.ACTIONS)
+        action_queue = [5]
+        state = copy(towers.INIT)
+        at = towers.Pole1
+        for (move, what, frm, to) in plan:
+            frm_pole = strips2_show.get_pole(state, frm)
+            to_pole = strips2_show.get_pole(state, to)
+            print what, frm, to, at, frm_pole, to_pole
+            if at != frm_pole:
+                action_queue += MOVES[(at, frm_pole)]
+            action_queue += CARRY_MOVES[(frm_pole, to_pole)]
+            move(state, what, frm, to)
+            at = to_pole
+        return action_queue
+
+    def start(self, time, observations):
+        """
+        return first action given the first observations
+        """
+        self.action_queue = self.queue_init()
+        return self.action_queue.pop(0)
+
+    def act(self, time, observations, reward):
+        """
+        return an action given the reward for the previous
+        action and the new observations
+        """
+        if len(self.action_queue) > 0:
+            return self.action_queue.pop(0)
+        else:
+            return 0
+
+    def end(self, time, reward):
+        """
+        receive the reward for the last observation
+        """
+        return True
+
+    def reset(self):
+        self.action_queue = self.queue_init()
+        return True
+
+    def destroy(self):
+        """
+        called when the agent is done
+        """
+        return True
+
+class TowerAgent3(AgentBrain):
+    """
+    An agent that uses a STRIPS-like planner to solve the Towers of Hanoi problem
+    """
+    def __init__(self):
+        AgentBrain.__init__(self) # have to make this call
+        self.action_queue = [5] # rotate left to reset state first
+
+    def initialize(self,init_info):
+        """
+        Create the agent.
+        init_info -- AgentInitInfo that describes the observation space (.sensors),
+                     the action space (.actions) and the reward space (.rewards)
+        """
+        self.action_info = init_info.actions
+        return True
+
+    def queue_init(self):
+        import subprocess
+        # solve for show (user can click through)
+        subproc = subprocess.Popen(['python', 'BlocksTower/strips.py', 'BlocksTower/towers2_strips.txt'], stdout=subprocess.PIPE)
+        plan = ''
+        while True:
+            try:
+                out = subproc.stdout.read(1)
+            except:
+                break
+            if out == '':
+                break
+            else:
+                plan += out
+        print plan
+        hl_actions = [] # high level action
+        for line in plan.split('\n'):
+            words = line.strip().split()
+            if len(words) == 3:
+                (what, frm, to) = words
+                hl_actions.append((what, frm, to))
+        
+        # use strips2 stuff for translating the output into low level actions
+        import strips2
+        import towers2 as towers
+        import strips2_show
+        
+        action_queue = [5]
+        state = copy(towers.INIT)
+        at = towers.Pole1
+        for (what, frm, to) in hl_actions:
+            frm_pole = strips2_show.get_pole(state, frm)
+            to_pole = strips2_show.get_pole(state, to)
+            print what, frm, to, at, frm_pole, to_pole
+            if at != frm_pole:
+                action_queue += MOVES[(at, frm_pole)]
+            action_queue += CARRY_MOVES[(frm_pole, to_pole)]
+            towers.Move(state, what, frm, to)
+            at = to_pole
+
+        return action_queue
+
+    def start(self, time, observations):
+        """
+        return first action given the first observations
+        """
+        self.action_queue = self.queue_init()
+        if len(self.action_queue) > 0:
+            return self.action_queue.pop(0)
+        else:
+            return 0
+
+    def act(self, time, observations, reward):
+        """
+        return an action given the reward for the previous
+        action and the new observations
+        """
+        if len(self.action_queue) > 0:
+            return self.action_queue.pop(0)
+        else:
+            return 0
+
+    def end(self, time, reward):
+        """
+        receive the reward for the last observation
+        """
+        return True
+
+    def reset(self):
+        self.action_queue = self.queue_init()
         return True
 
     def destroy(self):
--- mods/BlocksTower/constants.py
+++ mods/BlocksTower/constants.py
@@ -8,7 +8,24 @@ STEP_DELAY = 3.0 # number of seconds to wait between the sense-act-step repeats
 NUDGE_X = -5.0 # shift the island in +x by ...
 NUDGE_Y = -5.0 # shift the island in +y by ...
 WALL_TEMPLATE = "data/shapes/wall/BrickWall.xml"
-INITIAL_EPSILON = 0.1
 HISTORY_LENGTH = 5 # number of state-action pairs used to determine if the agent is stuck
 OBSTACLE_MASK = 1 #0b0001
 AGENT_MASK = 2 #0b0010
+
+import towers3 as towers
+
+# action primitives
+# move without getting stuff
+MOVES = { \
+    (towers.Pole1, towers.Pole2): [4, 1, 5], 
+    (towers.Pole1, towers.Pole3): [4, 1, 1, 5], 
+    (towers.Pole2, towers.Pole1): [5, 1, 4], 
+    (towers.Pole2, towers.Pole3): [4, 1, 5], 
+    (towers.Pole3, towers.Pole1): [5, 1, 1, 4], 
+    (towers.Pole3, towers.Pole2): [5, 1, 4] 
+}
+
+# move with pick up and put down
+CARRY_MOVES = {}
+for (source, dest) in MOVES:
+    CARRY_MOVES[(source, dest)] = [3] + MOVES[(source, dest)] + [2]
