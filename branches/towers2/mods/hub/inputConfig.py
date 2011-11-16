de3742a1d1d2d00e282b2c1ed016cf45f5b6afb0
ikarpov <ikarpov@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2011-11-16 17:12:31 +0000

    remove older branch

--- branches/towers2/mods/hub/inputConfig.py
+++
@@ -1,8 +0,0 @@
-from OpenNero import *
-
-def createInputMapping():
-    ioMap = PyIOMap()
-    # bind our keys
-    from common.module import openWiki
-    ioMap.BindKey( "KEY_F1", "onPress", openWiki('RunningOpenNero') )
-    return ioMap
