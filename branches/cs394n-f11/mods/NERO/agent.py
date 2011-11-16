80d7740e31710ff0f377f85abbc48260c4cb8b10
lmjohns3 <lmjohns3@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2011-11-16 20:17:11 +0000

    Refactor agent info into parent class. Simplify code for network_action a bit.

--- branches/cs394n-f11/mods/NERO/agent.py
+++ branches/cs394n-f11/mods/NERO/agent.py
@@ -12,15 +12,14 @@ class NeroAgent(object):
     def __init__(self, team=None, group='Agent'):
         self.team = team or module.getMod().curr_team
         self.group = group
-        self.info = self.create_agent_info()
+        self.info = OpenNero.AgentInitInfo(*self.agent_info_tuple())
 
     def get_team(self):
         return self.team
 
-    def create_agent_info(self):
+    def agent_info_tuple(self):
         abound = OpenNero.FeatureVectorInfo() # actions
         sbound = OpenNero.FeatureVectorInfo() # sensors
-        rbound = OpenNero.FeatureVectorInfo() # rewards
 
         # actions
         abound.add_continuous(-1, 1) # forward/backward speed
@@ -28,13 +27,14 @@ class NeroAgent(object):
 
         # sensor dimensions
         for a in range(constants.N_SENSORS):
-            sbound.add_continuous(0, 1);
+            sbound.add_continuous(0, 1)
 
-        # Rewards
-        for f in constants.FITNESS_DIMENSIONS:
-            rbound.add_continuous(-sys.float_info.max, sys.float_info.max)
+        return sbound, abound, module.rtneat_rewards()
 
-        return OpenNero.AgentInitInfo(sbound, abound, rbound)
+    def initialize(self, init_info):
+        self.actions = init_info.actions
+        self.sensors = init_info.sensors
+        return True
 
     def end(self, time, reward):
         return True
@@ -43,10 +43,13 @@ class NeroAgent(object):
         return True
 
 
-class RTNEATAgent(OpenNero.AgentBrain, NeroAgent):
+class RTNEATAgent(NeroAgent, OpenNero.AgentBrain):
     """
     rtNEAT agent
     """
+
+    ai = 'rtneat'
+
     def __init__(self):
         """
         Create an agent brain
@@ -55,15 +58,6 @@ class RTNEATAgent(OpenNero.AgentBrain, NeroAgent):
         # AgentBrainPtr by C++
         OpenNero.AgentBrain.__init__(self)
         NeroAgent.__init__(self)
-
-    def initialize(self, init_info):
-        """
-        Initialize an agent brain with sensor information
-        """
-        self.actions = init_info.actions # constraints for actions
-        self.sensors = init_info.sensors # constraints for sensors
-        self.group = "Agent"
-        return True
 
     def get_org(self):
         """
@@ -139,92 +133,67 @@ class RTNEATAgent(OpenNero.AgentBrain, NeroAgent):
         Take the current network
         Feed the sensors into it
         Activate the network to produce the output
-        Collect and interpret the outputs as valid maze actions
+        Collect and interpret the outputs as valid actions
         """
-        # make sure we have the right number of sensors
         assert(len(sensors) == constants.N_SENSORS)
-        # convert the sensors into the [0.0, 1.0] range
-        sensors = self.sensors.normalize(sensors)
-        # create the list of sensors
-        inputs = [sensor for sensor in sensors]
-        # add the bias value
-        inputs.append(constants.NEAT_BIAS)
-        # get the current organism we are using
+
         org = self.get_org()
-        # increment the lifetime counter
         org.time_alive += 1
-        # get the current network we are using
-        net = org.net
-        # load the list of sensors into the network input layer
-        net.load_sensors(inputs)
-        # activate the network
-        net.activate()
-        # get the list of network outputs
-        outputs = net.get_outputs()
 
-        # create a C++ vector for action values
+        org.net.load_sensors(
+            list(self.sensors.normalize(sensors)) + [constants.NEAT_BIAS])
+        org.net.activate()
+        outputs = org.net.get_outputs()
+
         actions = self.actions.get_instance()
-        # assign network outputs to action vector
-        for i in range(0,len(self.actions.get_instance())):
-            actions[i] = outputs[i]
-        # convert the action vector back from [0.0, 1.0] range
-        actions = self.actions.denormalize(actions)
-        #print "in:", inputs, "out:", outputs, "a:", actions
-        return actions
+        for i in range(len(self.actions.get_instance())):
+             actions[i] = outputs[i]
+        return self.actions.denormalize(actions)
 
 
-class Turret(OpenNero.AgentBrain, NeroAgent):
+class Turret(NeroAgent, OpenNero.AgentBrain):
     """
     Simple Rotating Turret
     """
+
+    ai = None
+
     def __init__(self):
         OpenNero.AgentBrain.__init__(self)
         NeroAgent.__init__(self, team=constants.OBJECT_TYPE_TEAM_1, group='Turret')
 
-    def initialize(self, init_info):
-        self.actions = init_info.actions
-        self.sensors = init_info.sensors
-        self.previous_fire =  0
-        return True
-
     def start(self, time, sensors):
-        self.org = None
-        self.net = None
-        self.state.label = "Turret"
+        self.state.label = 'Turret'
         a = self.actions.get_instance()
-        for x in range(len(self.actions)):
-            a[x] = 0
-            if x == 1:
-              a[x] = 0
+        a[0] = a[1] = 0
         return a
 
     def act(self, time, sensors, reward):
         a = self.actions.get_instance()
         a[0] = 0
-        a[1] = .15
+        a[1] = 0.1
         return a
 
 
-class QLearningAgent(OpenNero.QLearningBrain, NeroAgent):
-    def __init__(self, discount=0.8, learning_rate=0.8, epsilon=0.1, num_actions=3, num_states=5):
+class QLearningAgent(NeroAgent, OpenNero.QLearningBrain):
+    """
+    QLearning agent.
+    """
+
+    ai = 'qlearning'
+
+    def __init__(self,
+                 discount=0.8,
+                 learning_rate=0.8,
+                 epsilon=0.1,
+                 num_actions=3,
+                 num_states=5):
         OpenNero.QLearningBrain.__init__(
             self, discount, learning_rate, epsilon, num_actions, num_states)
         NeroAgent.__init__(self)
 
-    def create_agent_info(self):
-        abound = OpenNero.FeatureVectorInfo() # actions
-        sbound = OpenNero.FeatureVectorInfo() # sensors
-        rbound = OpenNero.FeatureVectorInfo() # rewards
-
-        # actions
-        abound.add_continuous(-1, 1) # forward/backward speed
-        abound.add_continuous(-0.2, 0.2) # left/right turn (in radians)
-
-        # sensor dimensions
-        for a in range(constants.N_SENSORS):
-            sbound.add_continuous(0, 1);
-
-        # Rewards -- just one dimension for reinforcement learning
+    def agent_info_tuple(self):
+        sbound, abound, _ = NeroAgent.agent_info_tuple(self)
+        rbound = OpenNero.FeatureVectorInfo() # single-dimension rewards
         rbound.add_continuous(-sys.float_info.max, sys.float_info.max)
-
-        return OpenNero.AgentInitInfo(sbound, abound, rbound)
+        return sbound, abound, rbound
