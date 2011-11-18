467dbd4b2bf338348975f2d6f0533484d2d14dfe
lmjohns3 <lmjohns3@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2011-11-18 22:37:12 +0000

    Do not run separate menu process when --headless.

--- branches/cs394n-f11/mods/NERO_Battle/main.py
+++ branches/cs394n-f11/mods/NERO_Battle/main.py
@@ -1,24 +1,24 @@
-#import all agent implementations
-import NERO.agent
-
-#import all client and server scripts
-import NERO_Battle.module as module
-import NERO.client as client
 import common
 import common.menu_utils
+import NERO.agent
+import NERO.client as client
+import NERO.constants as constants
+import NERO_Battle.module as module
+import OpenNero
 
 script_server = None
 
 def ModMain():
-    global script_server
     module.getMod()  # initialize the NERO_Battle module.
-    script_server = common.menu_utils.GetScriptServer()
-    common.startScript("NERO_Battle/menu.py")
     client.ClientMain()
 
 def ModTick(dt):
-    if script_server is None:
+    global script_server
+    if OpenNero.getAppConfig().rendertype == 'null':
         return
+    if script_server is None:
+        script_server = common.menu_utils.GetScriptServer()
+        common.startScript("NERO_Battle/menu.py")
     data = script_server.read_data()
     while data:
         module.parseInput(data.strip())
