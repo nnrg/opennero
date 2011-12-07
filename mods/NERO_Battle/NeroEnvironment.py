7afe8cc667ed781f1b839630235d1271129a1696
lmjohns3 <leif@cs.utexas.edu>
2011-12-07 18:34:41 +0000

    Fix up several bugs in battle mode, particularly relating to damage assessment. Remove agents from the simulation altogether when they accumulate too much damage. Wait until all teams have spawned before making damage assessments. Make winning agents jump.

--- mods/NERO_Battle/NeroEnvironment.py
+++ mods/NERO_Battle/NeroEnvironment.py
@@ -10,7 +10,7 @@ class NeroEnvironment(NERO.NeroEnvironment.NeroEnvironment):
     def __init__(self):
         NERO.NeroEnvironment.NeroEnvironment.__init__(self)
         self.script = 'NERO_Battle/menu.py'
-        self.print_damage = 0
+        self.print_damage = -1
 
     def step(self, agent, action):
         """
@@ -22,38 +22,39 @@ class NeroEnvironment(NERO.NeroEnvironment.NeroEnvironment):
         reward = NERO.NeroEnvironment.NeroEnvironment.step(self, agent, action)
 
         live_agents = sum(len(t) for t in self.teams.itervalues())
+
+        # wait until all agents have spawned before we keep track of damage.
+        if self.print_damage < 0 and live_agents < 2 * constants.pop_size:
+            return reward
+
+        # print out damages every loop through the entire population.
         self.print_damage += 1
         if self.print_damage >= live_agents:
             self.print_damage = 0
-
-        # if the returned reward includes a "hit" value, but it is 0, no damage
-        # has occurred, so just return.
-        hit_index = constants.FITNESS_INDEX[constants.FITNESS_HIT_TARGET]
-        if len(reward) >= hit_index and not reward[hit_index]:
-            return reward
-
-        # if the returned reward does not include a distinct "hit" value, we
-        # print out damages every loop through the entire population.
-        if len(reward) == 1 and self.print_damage > 0:
+        else:
             return reward
 
         damages = {}
         for team in constants.TEAMS:
             damages[team] = 0
             agents = self.teams.get(team, set())
-            if agents:
-                damage = sum(self.get_state(a).total_damage for a in agents)
+            agent_damages = [self.get_state(a).total_damage for a in agents]
+            if any(d < self.hitpoints for d in agent_damages):
+                damage = sum(agent_damages)
                 damage += (constants.pop_size - len(agents)) * self.hitpoints
                 damages[team] = damage
             else:
-                foe = constants.OBJECT_TYPE_TEAM_0
-                if team == foe:
-                    foe = constants.OBJECT_TYPE_TEAM_1
-                s = constants.TEAM_LABELS[foe] + ' team wins!!!'
+                damages[team] = constants.pop_size * self.hitpoints
+                winner = constants.OBJECT_TYPE_TEAM_0
+                if team == winner:
+                    winner = constants.OBJECT_TYPE_TEAM_1
+                s = constants.TEAM_LABELS[winner] + ' team wins!!!'
                 print s
                 if not constants.getDisplayHint():
                     OpenNero.setWindowCaption(s)
                 OpenNero.disable_ai()
+                for a in self.teams[winner]:
+                    self.set_animation(a, self.get_state(a), 'jump')
 
         if len(damages) == 2:
             ss = []
@@ -68,15 +69,19 @@ class NeroEnvironment(NERO.NeroEnvironment.NeroEnvironment):
 
         return reward
 
+    def reset(self, agent):
+        """
+        reset the environment to its initial state
+        """
+        # NOOP
+        return True
+
     def is_episode_over(self, agent):
         """
         is the current episode over for the agent?
         """
-        state = self.get_state(agent)
         assert self.hitpoints > 0
-        if state.total_damage >= self.hitpoints:
-            # agent has died, send it to heaven.
-            agent.state.position = OpenNero.Vector3f(100000, 100000, 100000)
-            state.pose = (100000, 100000, 0)
-            self.teams[agent.get_team()].discard(agent)
+        if self.get_state(agent).total_damage >= self.hitpoints:
+            self.remove_agent(agent)
+            return True
         return False
