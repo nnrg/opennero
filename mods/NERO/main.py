feb65d2b3fa3a1eead45eb56b142224ff07a9c90
ikarpov <ikarpov@gmail.com>
2011-12-09 15:28:10 +0000

    change NERO/main.py to allow to run in headless mode and be more like NERO_Battle

--- mods/NERO/main.py
+++ mods/NERO/main.py
@@ -4,15 +4,26 @@ import NERO.client
 import NERO.agent
 import common
 import common.menu_utils
+import OpenNero
 
 def ModMain():
     NERO.client.ClientMain()
 
-script_server = common.menu_utils.GetScriptServer()
+script_server = None
 
 def ModTick(dt):
-    common.startScript("NERO/menu.py")
+    global script_server
+    # don't start the external menu if we are running in headless mode!
+    if OpenNero.getAppConfig().rendertype == 'null':
+        return
+    if script_server is None:
+        script_server = common.menu_utils.GetScriptServer()
+        common.startScript("NERO/menu.py")
     data = script_server.read_data()
     while data:
         NERO.module.parseInput(data.strip())
         data = script_server.read_data()
+
+def StartEvolving():
+    mod = NERO.module.getMod()
+    mod.deploy('rtneat')
