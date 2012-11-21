931c78e6c64595dd75620274be88e64c22e7b811
ikarpov <ikarpov@gmail.com>
2012-11-21 18:22:50 +0000

    better logging

--- java/OpenNERO/src/main/java/edu/utexas/cs/nn/opennero/Message.java
+++ java/OpenNERO/src/main/java/edu/utexas/cs/nn/opennero/Message.java
@@ -1,7 +1,10 @@
 package edu.utexas.cs.nn.opennero;
 
+import java.io.StringWriter;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
 
 /**
  *
@@ -11,20 +14,38 @@ import org.simpleframework.xml.Root;
 public class Message {
     @Element
     private Content content;
+
     public Message() {}
+
     public Message(Content content) {
         this.content = content;
     }
+
     public Content getContent() { return content; }
+
     public interface Content {
     }
+
     @Override
     public String toString() {
         return this.content.toString();
     }
+    
+    private static final Serializer serializer = new Persister();
+    
+    public String toXML() {
+        StringWriter sw = new StringWriter();
+        try {
+            serializer.write(this, sw);
+        } catch (Exception ex) {
+            return "";
+        }
+        return sw.toString();
+    }
+    
     public static void main(String[] args) {
         Content c = new Command("a", "b");
         Message m = new Message(c);
-        System.out.println(SocketClient.instance.toXml(m));
+        System.out.println(m.toXML());
     }
 }
