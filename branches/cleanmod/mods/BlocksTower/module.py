3ac92d42151a9fc4a62d671fa57336cb728bac02
reza.mahjourian <reza.mahjourian@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2014-08-08 09:08:40 +0000

    Showing cubes early on in BlocksTower mod.

--- branches/cleanmod/mods/BlocksTower/module.py
+++ branches/cleanmod/mods/BlocksTower/module.py
@@ -52,21 +52,26 @@ class TowerMod:
     def num_towers(self):
         return num_towers
 
-    def start_tower1(self):
+    def start_tower1(self): #Problem reduction
         """ start the tower demo """
         self.num_towers = 3
         OpenNero.disable_ai()
         self.stop_agent()
-        self.set_environment(TowerEnvironment())
+        env = TowerEnvironment()
+        env.initialize_blocks()
+        self.set_environment(env)
         self.agent_id = common.addObject("data/shapes/character/BlocksRobot.xml", OpenNero.Vector3f(constants.GRID_DX, constants.GRID_DY, 2), type=constants.AGENT_MASK, scale=OpenNero.Vector3f(3,3,3))
         OpenNero.enable_ai()
 
-    def start_tower2(self):
+    def start_tower2(self): #State-space search
         """ start the tower demo """
-        self.num_towers = 2
+        self.num_towers = 3
         OpenNero.disable_ai()
         self.stop_agent()
-        self.set_environment(TowerEnvironment())
+        env = TowerEnvironment()
+        env.initialize_blocks()
+        self.set_environment(env)
+        #self.set_environment(TowerEnvironment())
         self.agent_id = common.addObject("data/shapes/character/BlocksRobot2.xml", OpenNero.Vector3f(constants.GRID_DX, constants.GRID_DY, 2), type=constants.AGENT_MASK, scale=OpenNero.Vector3f(3,3,3))
         OpenNero.enable_ai()
 
@@ -75,7 +80,10 @@ class TowerMod:
         self.num_towers = 2
         OpenNero.disable_ai()
         self.stop_agent()
-        self.set_environment(TowerEnvironment())
+        env = TowerEnvironment()
+        env.initialize_blocks()
+        self.set_environment(env)
+        #self.set_environment(TowerEnvironment())
         self.agent_id = common.addObject("data/shapes/character/BlocksRobot3.xml", OpenNero.Vector3f(constants.GRID_DX, constants.GRID_DY, 2), type=constants.AGENT_MASK, scale=OpenNero.Vector3f(3,3,3))
         OpenNero.enable_ai()
         
@@ -84,7 +92,10 @@ class TowerMod:
         self.num_towers = 3
         OpenNero.disable_ai()
         self.stop_agent()
-        self.set_environment(TowerEnvironment())
+        env = TowerEnvironment()
+        env.initialize_blocks()
+        self.set_environment(env)
+        #self.set_environment(TowerEnvironment())
         self.agent_id = common.addObject("data/shapes/character/BlocksRobot4.xml", OpenNero.Vector3f(constants.GRID_DX, constants.GRID_DY, 2), type=constants.AGENT_MASK, scale=OpenNero.Vector3f(3,3,3))
         OpenNero.enable_ai()
         
@@ -93,7 +104,10 @@ class TowerMod:
         self.num_towers = 3
         OpenNero.disable_ai()
         self.stop_agent()
-        self.set_environment(TowerEnvironment())
+        env = TowerEnvironment()
+        env.initialize_blocks()
+        self.set_environment(env)
+        #self.set_environment(TowerEnvironment())
         self.agent_id = common.addObject("data/shapes/character/BlocksRobot5.xml", OpenNero.Vector3f(constants.GRID_DX, constants.GRID_DY, 2), type=constants.AGENT_MASK, scale=OpenNero.Vector3f(3,3,3))
         OpenNero.enable_ai()
         
