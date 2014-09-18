c0d4c5acb24a90186431e9e4416aa47315b14f0d
reza.mahjourian <reza.mahjourian@25e8a098-f32b-11de-8b01-fd14ac940ccb>
2014-09-18 16:11:49 +0000

    Renamed BlocksHanoi to TowerofHanoi.

--- mods/TowerofHanoi/main.py
+++ mods/TowerofHanoi/main.py
@@ -1,5 +1,5 @@
 # OpenNero will execute ModMain when this mod is loaded
-from BlocksHanoi.client import ClientMain
+from TowerofHanoi.client import ClientMain
 
 def ModMain(mode = ""):
     ClientMain(mode)
